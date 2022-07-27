d1056fee52b3058ba63c2292a13f007594731496
jonihoffi <jonas.luc.hoffmann@gmail.com>
2022-07-27 19:23:25 +0200

    Check some errors

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
 
-        result = search("Google", num=15, lang="de")
+        result = search("Google", num=1, lang="de")
 
         for url in result:
             print(url)
