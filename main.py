654364cbdeb16f703a8c69fb49f9071807170a76
jonihoffi <jonas.luc.hoffmann@gmail.com>
2022-07-27 19:14:48 +0200

    Complete change.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import requests
-import urllib
-import pandas as pd
-from requests_html import HTMLSession
-from requests_html import HTML
+from googlesearch import search
 
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
@@ -24,48 +20,14 @@ class KeywordQueryEventListener(EventListener):
     def on_event(self, event, extension):
         items = []
 
-        def get_source(url):
-            """Return the source code for the provided URL.
+        result = search("Google", num=15, lang="de")
 
-            Args:
-                url (string): URL of the page to scrape.
-
-            Returns:
-                response (object): HTTP response object from requests_html.
-            """
-
-            try:
-                session = HTMLSession()
-                response = session.get(url)
-                return response
-
-            except requests.exceptions.RequestException as e:
-                print(e)
-
-
-        def scrape_google(query):
-            query = urllib.parse.quote_plus(query)
-            response = get_source("https://www.google.co.uk/search?q=" + query)
-
-            links = list(response.html.absolute_links)
-            google_domains = ('https://www.google.',
-                              'https://google.',
-                              'https://webcache.googleusercontent.',
-                              'http://webcache.googleusercontent.',
-                              'https://policies.google.',
-                              'https://support.google.',
-                              'https://maps.google.')
-
-            for url in links[:]:
-                if url.startswith(google_domains):
-                    links.remove(url)
-                else:
-                    items.append(ExtensionResultItem(icon='images/icon.png',
-                                                     name=url,
-                                                     description=url,
-                                                     on_enter=HideWindowAction()))
-
-            return links
+        for url in result:
+            print(url)
+            items.append(ExtensionResultItem(icon='images/icon.png',
+                                             name=url,
+                                             description=url,
+                                             on_enter=HideWindowAction()))
 
         return RenderResultListAction(items)
 
